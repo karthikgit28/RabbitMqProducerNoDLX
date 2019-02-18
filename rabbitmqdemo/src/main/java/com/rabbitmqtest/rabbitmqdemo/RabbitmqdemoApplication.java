f57c753ada7a7d0d0346527608ab9b81c113f613
karthikgit28 <karthik.jec08@gmail.com>
2019-02-18 14:54:08 +0530

    first

---
+++ rabbitmqdemo/src/main/java/com/rabbitmqtest/rabbitmqdemo/RabbitmqdemoApplication.java
@@ -0,0 +1,25 @@
+package com.rabbitmqtest.rabbitmqdemo;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqdemoApplication implements CommandLineRunner{
+	
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqdemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		rabbitTemplate.convertAndSend("TestExchange", "TestRouting", "This is Karthik");
+	}
+
+}
+
